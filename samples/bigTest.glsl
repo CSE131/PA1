f9c8397d8d4d44b666fe4f919e014e9aea46c209
Gurkirat Singh <tablagurkirat@yahoo.com>
2017-01-21 14:08:28 -0800

    more tests

---
+++ samples/bigTest.glsl
@@ -0,0 +1,62 @@
+1e10f
+1e2
+1e0
+10e10f
+10E10f
+10e100
+1.e1
+1.e13
+1.564e3
+123e
+123.123e+3
+123.123e-3
+123.123E+3
+123.123e+3f
+123.123e-3f
+123.123E+3f
+123.123E-3f
+123e3
+12345
+// more comment
+// comment comment comment
+// blah blah blah
+// ok
+vec2 vec2 vec2
+123.
+0.12345f
+0.123F
+.123
+-123
+123u
+123.12345Ula
+123.12345ula
+_abc
+1e10f
+1e2
+1e0
+e12345
+true false 
+ivec2.2
+342.342342u
+3423423U
+a.a
+*=
+/=
++=
+-=
+
+()[]{}
+.:=;-+*/<>?
+
+ivec2
+ivec3
+ivec4
+
+bvec2
+bvec3
+bvec4
+
+mat2
+mat3
+mat4
+struct
